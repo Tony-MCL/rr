b005a12a0ccb37786135001b8c8503ae18b73e6c
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-18 12:21:51 +0200

    Validate required level metadata

--- core/level_configuration.gd
+++ core/level_configuration.gd
@@ -9,3 +9,22 @@ extends Resource
 
 @export_category("Starting Ammunition")
 @export_range(0, 999, 1) var starting_ammunition: int = 5
+
+
+func get_validation_errors() -> PackedStringArray:
+	var errors := PackedStringArray()
+
+	if level_id < 1:
+		errors.append("level_id must be 1 or greater")
+	if level_pack < 1:
+		errors.append("level_pack must be 1 or greater")
+	if content_version < 1:
+		errors.append("content_version must be 1 or greater")
+	if starting_ammunition < 0:
+		errors.append("starting_ammunition cannot be negative")
+
+	return errors
+
+
+func is_valid() -> bool:
+	return get_validation_errors().is_empty()
