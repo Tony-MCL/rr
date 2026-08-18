7fea14924f01d96334158a3ff0aba3bd4f2ec450
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-18 20:45:57 +0200

    Add ConstructionGroup component

---
+++ components/constructions/construction_group.gd
@@ -0,0 +1,9 @@
+class_name ConstructionGroup
+extends Node2D
+
+@export_category("Construction")
+@export var construction_id: StringName = &"construction"
+
+
+func get_construction_id() -> StringName:
+	return construction_id
