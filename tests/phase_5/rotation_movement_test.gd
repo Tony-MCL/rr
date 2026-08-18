9a572e0eb86dc3764c730aa59433f63bc2410546
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-18 21:08:03 +0200

    Fix rotation movement test typing

--- tests/phase_5/rotation_movement_test.gd
+++ tests/phase_5/rotation_movement_test.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var movement: RotationMovement = $RotationMovement
+@onready var marker: Node2D = $RotationMovement/Marker
 @onready var status_label: Label = $StatusLabel
 
 var _initial_marker_position: Vector2
@@ -8,7 +9,7 @@ var _completed: bool = false
 
 
 func _ready() -> void:
-	_initial_marker_position = $RotationMovement/Marker.global_position
+	_initial_marker_position = marker.global_position
 	await get_tree().create_timer(2.2).timeout
 	_run_test()
 
@@ -18,14 +19,14 @@ func _run_test() -> void:
 		return
 	_completed = true
 
-	var marker_moved := $RotationMovement/Marker.global_position.distance_to(_initial_marker_position) > 20.0
-	var radius_preserved := is_equal_approx($RotationMovement/Marker.position.length(), 70.0)
-	var rotated := absf(rad_to_deg(movement.rotation - movement.get_initial_rotation())) > 120.0
+	var marker_moved: bool = marker.global_position.distance_to(_initial_marker_position) > 20.0
+	var radius_preserved: bool = is_equal_approx(marker.position.length(), 70.0)
+	var rotated: bool = absf(rad_to_deg(movement.rotation - movement.get_initial_rotation())) > 120.0
 
 	movement.reset_movement()
-	var reset_ok := is_equal_approx(movement.rotation, movement.get_initial_rotation() + TAU * movement.start_phase)
+	var reset_ok: bool = is_equal_approx(movement.rotation, movement.get_initial_rotation() + TAU * movement.start_phase)
 
-	var passed := marker_moved and radius_preserved and rotated and reset_ok
+	var passed: bool = marker_moved and radius_preserved and rotated and reset_ok
 	status_label.text = "ROTATION MOVEMENT TEST: %s\nMarker moved: %s\nRadius preserved: %s\nRotation progressed: %s\nReset phase: %s" % [
 		"PASS" if passed else "FAIL",
 		"YES" if marker_moved else "NO",
