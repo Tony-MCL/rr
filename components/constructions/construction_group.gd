38b97bb82585d44326d88d3411a59c0bac97d03d
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-18 20:56:21 +0200

    Add shared construction pivot behavior

--- components/constructions/construction_group.gd
+++ components/constructions/construction_group.gd
@@ -7,3 +7,11 @@ extends Node2D
 
 func get_construction_id() -> StringName:
 	return construction_id
+
+
+func get_pivot_global_position() -> Vector2:
+	return global_position
+
+
+func set_pivot_rotation_degrees(angle_degrees: float) -> void:
+	rotation_degrees = angle_degrees
