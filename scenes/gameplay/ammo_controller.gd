bbdd845c71554dde8d6ed854a0775509f71b0dd7
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-18 07:12:44 +0200

    Create AmmoController

---
+++ scenes/gameplay/ammo_controller.gd
@@ -0,0 +1,38 @@
+extends Node
+
+signal ammunition_changed(current_ammunition: int)
+
+var _current_ammunition: int = 0
+
+
+func get_ammunition() -> int:
+	return _current_ammunition
+
+
+func set_ammunition(amount: int) -> void:
+	_current_ammunition = maxi(amount, 0)
+	ammunition_changed.emit(_current_ammunition)
+	print("AMMUNITION: %d" % _current_ammunition)
+
+
+func has_ammunition() -> bool:
+	return _current_ammunition > 0
+
+
+func consume_one() -> bool:
+	if not has_ammunition():
+		return false
+
+	_current_ammunition -= 1
+	ammunition_changed.emit(_current_ammunition)
+	print("AMMUNITION: %d" % _current_ammunition)
+	return true
+
+
+func add_ammunition(amount: int = 1) -> void:
+	if amount <= 0:
+		return
+
+	_current_ammunition += amount
+	ammunition_changed.emit(_current_ammunition)
+	print("AMMUNITION: %d" % _current_ammunition)
