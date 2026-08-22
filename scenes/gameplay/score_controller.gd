51244ad13119b541f26ed30db01c98c28158f3df
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-22 08:41:18 +0200

    Add global zone score values

--- scenes/gameplay/score_controller.gd
+++ scenes/gameplay/score_controller.gd
@@ -2,11 +2,22 @@ extends Node
 
 signal score_changed(current_score: int)
 
+const ZONE_VALUES := {
+	1: 10,
+	2: 20,
+	3: 30,
+	4: 40,
+}
+
 var _current_score: int = 0
 
 
 func get_score() -> int:
 	return _current_score
+
+
+func get_zone_value(zone_id: int) -> int:
+	return int(ZONE_VALUES.get(zone_id, 0))
 
 
 func add_score(points: int) -> void:
